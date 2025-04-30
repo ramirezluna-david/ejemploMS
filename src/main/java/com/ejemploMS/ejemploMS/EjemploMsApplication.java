16b2cb0474b58cadd6673e5c4f4537f63e579f2c
David Alejandro Ramírez Luna <davidrrluna@gmail.com>
2025-04-30 12:31:40 -0400

    first commit

---
+++ src/main/java/com/ejemploMS/ejemploMS/EjemploMsApplication.java
@@ -0,0 +1,13 @@
+package com.ejemploMS.ejemploMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemploMS/ejemploMS/EjemploMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemploMS.ejemploMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
